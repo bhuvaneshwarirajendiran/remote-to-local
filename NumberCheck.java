24c31b2742129c3820bfaa82c3e8af1bb74e7e3c
bhuvaneshwarirajendiran <bhuvaneshwari.rajendiran@gmail.com>
2024-07-12 22:04:15 +0530

    new file added

---
+++ NumberCheck.java
@@ -0,0 +1,14 @@
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        int number = 5; // You can change this value to test different cases
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
