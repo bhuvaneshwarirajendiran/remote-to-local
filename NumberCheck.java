f424d0b856da66054d1f3ac0640ccbf8a3334b2c
bhuvaneshwarirajendiran <bhuvaneshwari.rajendiran@gmail.com>
2024-07-12 22:31:54 +0530

    number changed from 5 to -10

--- NumberCheck.java
+++ NumberCheck.java
@@ -1,6 +1,6 @@
 public class NumberCheck {
     public static void main(String[] args) {
-        int number = 5; // You can change this value to test different cases
+        int number = -10; // You can change this value to test different cases
 
         if (number > 0) 
             System.out.println("The number is positive.");
